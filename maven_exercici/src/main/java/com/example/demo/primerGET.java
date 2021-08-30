e6c7539f965847e3956bb6bc9af015cae303429f
Miquel <76565907+mpieb@users.noreply.github.com>
2021-08-30 17:04:56 +0200

    Add files via upload

---
+++ maven_exercici/src/main/java/com/example/demo/primerGET.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class primerGET
+ */
+@WebServlet("/")
+public class primerGET extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public primerGET() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String path = getServletContext().getRealPath("Miquel");
+		response.getWriter().append("<h1> Hola Mon! </h1> " + path);
+	}
+
+}
